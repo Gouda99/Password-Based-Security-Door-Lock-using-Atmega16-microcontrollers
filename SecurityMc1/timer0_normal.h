7de4bbcfb00d5489b78134862aaa0aa5c29111b9
Gouda99 <69635994+Gouda99@users.noreply.github.com>
2020-12-27 16:31:50 +0200

    Add files via upload

---
+++ SecurityMc1/timer0_normal.h
@@ -0,0 +1,11 @@
+/* ---------------------------------------------------------------------------------------------------
+ [FILE NAME]: <timer0_normal.h>
+ [AUTHOR(S)]: <GOUDA'>
+ [DESCRIPTION]: <Contains functions prototypes to handle the timer0 in normal mode>
+ ---------------------------------------------------------------------------------------------------*/
+#ifndef TIMER0_NORMAL_H_
+#define TIMER0_NORMAL_H_
+
+void timer0_init_normal_mode(void);
+
+#endif /* TIMER0_NORMAL_H_ */
